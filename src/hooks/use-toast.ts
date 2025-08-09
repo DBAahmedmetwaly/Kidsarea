a2dc1198aa3735ba0bb7d6a3d078220827d5419c
Ahmed Metwaly <luisarabss@gmail.com>
2025-08-09 14:57:25 +0000

    مدة ظهور الاشعارات يجب ان تكون ثانية واحدة  او امكانية التحكم بها

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -158,6 +158,7 @@ function toast({ ...props }: Toast) {
       ...props,
       id,
       open: true,
+      duration: 1000, // Set duration to 1 second
       onOpenChange: (open) => {
         if (!open) dismiss()
       },
