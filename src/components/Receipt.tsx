0c03060e3423ffd71cc8b3a695c447dfe9519053
Ahmed Metwaly <luisarabss@gmail.com>
2025-09-05 16:04:18 +0000

    الايصال الآن ممتاز اريد ايضا ظهور اسماء الاطفال الوجودة فى الإيصال ووقت

--- src/components/Receipt.tsx
+++ src/components/Receipt.tsx
@@ -100,7 +100,7 @@ export const PosReceipt = React.forwardRef<HTMLDivElement, PosReceiptProps>(({
             {show('showCashierName') && <div className="grid grid-cols-2"><span>الكاشير:</span> <span className='text-left'>{cashierName}</span></div>}
             {show('showParentName') && <div className="grid grid-cols-2"><span>ولي الأمر:</span> <span className='text-left'>{parentName}</span></div>}
             {show('showParentName') && <div className="grid grid-cols-2"><span>الهاتف:</span> <span className='text-left font-mono'>{(phoneNumbers || []).join('/')}</span></div>}
-            <div className="grid grid-cols-2"><span>الأطفال ({children.length}):</span> <span className='text-left'>{children.map(c => c.name).join(', ')}</span></div>
+            {show('showChildName') && <div className="grid grid-cols-2"><span>الأطفال ({children.length}):</span> <span className='text-left'>{children.map(c => c.name).join(', ')}</span></div>}
        </div>
 
        {/* Session Details */}
