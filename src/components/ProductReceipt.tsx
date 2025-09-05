c3127205217b3a62720e70bc9d45599df66f1d09
Ahmed Metwaly <luisarabss@gmail.com>
2025-09-05 15:33:53 +0000

    قسم الإيصال إلى عمدة أسم الباقة الكمية  السعر  الاجمالى

--- src/components/ProductReceipt.tsx
+++ src/components/ProductReceipt.tsx
@@ -1,5 +1,4 @@
 
-
 'use client';
 
 import React from 'react';
@@ -45,17 +44,17 @@ export const ProductReceipt = React.forwardRef<HTMLDivElement, ProductReceiptPro
 
       <div className="space-y-0.5 border-t border-b border-dashed border-gray-400 py-1 my-1">
         <div className="grid grid-cols-12 text-xs font-bold">
-            <span className="col-span-5 text-right">الصنف</span>
+            <span className="col-span-6 text-right">الصنف</span>
             <span className="col-span-2 text-center">الكمية</span>
             <span className="col-span-2 text-center">السعر</span>
-            <span className="col-span-3 text-left">الإجمالي</span>
+            <span className="col-span-2 text-left">الإجمالي</span>
         </div>
          {items.map((item, index) => (
             <div key={index} className="grid grid-cols-12 text-xs">
-                <span className="col-span-5 text-right">{item.name}</span>
+                <span className="col-span-6 text-right">{item.name}</span>
                 <span className="col-span-2 text-center">{item.quantity}</span>
                 <span className="col-span-2 text-center font-mono">{item.price.toFixed(2)}</span>
-                <span className="col-span-3 text-left font-mono">{`ج.م ${(item.price * item.quantity).toFixed(2)}`}</span>
+                <span className="col-span-2 text-left font-mono">{(item.price * item.quantity).toFixed(2)}</span>
             </div>
          ))}
       </div>
