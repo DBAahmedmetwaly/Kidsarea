9ba5689d00a53506954797eeceacdb45301cc055
Ahmed Metwaly <luisarabss@gmail.com>
2025-09-06 08:01:03 +0000

    pos:1  Access to manifest at 'https://workstations.cloud.google.com/devs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,6 @@ import { PermissionsProvider } from '@/context/PermissionsContext';
 export const metadata: Metadata = {
   title: 'FunTrack Manager',
   description: 'Kids Play Area Management System',
-  manifest: null,
 };
 
 export default function RootLayout({
