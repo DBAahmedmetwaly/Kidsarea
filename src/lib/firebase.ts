4ea532ce5329fd92ab156b13adf03efd61bb2f1e
Ahmed Metwaly <luisarabss@gmail.com>
2025-09-08 19:20:02 +0000

    ملف .evnt ليس بداخلة شئ غير هذا السطر فقط NEXT_PUBLIC_FIREBASE_API_KEY

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,14 +3,14 @@ import { initializeApp, getApp, getApps } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA0r91-Fpc3bkIh-1yAGtegCJtXOcY-HKA",
-  authDomain: "kids-aria.firebaseapp.com",
-  databaseURL: "https://kids-aria-default-rtdb.firebaseio.com",
-  projectId: "kids-aria",
-  storageBucket: "kids-aria.firebasestorage.app",
-  messagingSenderId: "711487865094",
-  appId: "1:711487865094:web:744939809e46b38a361e87",
-  measurementId: "G-T1SR0E84N5"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
