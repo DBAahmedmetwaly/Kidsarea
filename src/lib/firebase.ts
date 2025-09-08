68764704c8f2cbfc75a4f8922d0d4473ca6505ca
Ahmed Metwaly <luisarabss@gmail.com>
2025-09-08 19:14:31 +0000

    لم اتمكن من الاتصال بقاعدة البيانات ارشدنى لخطوات

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,7 +6,7 @@ const firebaseConfig = {
   projectId: "funtrack-manager",
   appId: "1:251373291254:web:baa6647ccb8630f8028ed7",
   storageBucket: "funtrack-manager.appspot.com",
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  apiKey: "AIzaSyBf0jW0P5BsoVyx62NIE9DK3FI11uIRynU",
   authDomain: "funtrack-manager.firebaseapp.com",
   measurementId: "",
   messagingSenderId: "251373291254",
